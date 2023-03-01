41b82ba1e857172db64ca6aea3b3f30bf24c000b
bretheskevin <bretheskevin@gmail.com>
2023-03-01 19:56:57 +0100

    Fix service worker going offline

--- background.js
+++ background.js
@@ -1,60 +1,257 @@
-let debug = false;
+importScripts("websocketawake.js");
 
-let previousData = {
-    "isLive": false,
-    "lastLive": null,
-    "title": ""
-}
+const DEBUG_MODE = false;
+const NOTIFICATION_MESSAGE = "Donc là tu vois la notif, mais tu cliques pas ? Rejoins-nous !";
+const NOTIFICATION_OPTIONS = {
+    "type": "basic",
+    "iconUrl": "/assets/128.png",
+};
 
-const ws = new WebSocket(debug ? "ws://localhost:4001" : "ws://bretheskevin.fr:4001");
+const ws = new WebSocket(DEBUG_MODE ? "ws://localhost:4001" : "ws://bretheskevin.fr:4001");
 
-ws.addEventListener('open', (event) => {
+ws.addEventListener('open', () => {
     console.log('WebSocket connection opened');
 });
 
 ws.addEventListener('message', async (event) => {
-    getPreviousData()
+    const previousData = await getPreviousData();
+    const currentData = JSON.parse(event.data);
 
-    let data = JSON.parse(event.data);
-
-    if (shouldSendNotification(previousData, data)) {
-        setPreviousData(data);
+    if (shouldSendNotification(previousData, currentData)) {
+        await setPreviousData(currentData);
 
         browser.notifications.create({
-            "type": "basic",
-            "iconUrl": "/assets/128.png",
-            "title": data.title,
-            "message": "Donc là tu vois la notif, mais tu cliques pas ? Rejoins-nous !"
+            ...NOTIFICATION_OPTIONS,
+            "title": currentData.title,
+            "message": NOTIFICATION_MESSAGE
         });
-    } else {
-        console.log("No notification sent");
-        console.log("Previous data: ", previousData);
-        console.log("Current data: ", data);
     }
 });
 
-browser.notifications.onClicked.addListener(function(event){
+browser.notifications.onClicked.addListener(() => {
     browser.tabs.create({url: 'https://kick.com/thevivi'});
 });
 
-function getPreviousData() {
-    browser.storage.sync.get("thevivi", (result) => {
-        previousData = result.thevivi || previousData;
-    });
+async function getPreviousData() {
+    const result = await browser.storage.local.get("thevivi");
+    return result.thevivi || {
+        "isLive": false,
+        "lastLive": null,
+        "title": ""
+    };
 }
 
-function setPreviousData(data) {
-    previousData = data;
-    browser.storage.sync.set({thevivi: previousData});
+async function setPreviousData(data) {
+    await browser.storage.local.set({thevivi: data});
 }
 
-function shouldSendNotification(previousData, data) {
-    return data.isLive &&
-        !previousData.isLive &&
-        (data.lastLive !== previousData.lastLive) &&
-        (data.lastLive !== null);
+function shouldSendNotification(previousData, currentData) {
+    return (currentData.lastLive > previousData.lastLive) && (currentData.lastLive !== null);
 }
 
-setInterval(() => {
-    console.log("Keeping service worker alive");
-}, 1000 * 60 * 4);
+
+// Keep the service worker alive (thanks google  for making things more complicated than they should be)
+
+const INTERNAL_TESTALIVE_PORT = "DNA_Internal_alive_test";
+
+const startSeconds = 1;
+const nextSeconds = 25;
+const SECONDS = 1000;
+const DEBUG = false;
+
+let alivePort = null;
+let isFirstStart = true;
+let isAlreadyAwake = false;
+let timer = startSeconds*SECONDS;
+
+let firstCall;
+let lastCall;
+
+let wakeup = undefined;
+let wsTest = undefined;
+let wCounter = 0;
+
+const starter = `-------- >>> ${convertNoDate(Date.now())} UTC - Service Worker with HIGHLANDER DNA is starting <<< --------`;
+
+// Websocket test
+webSocketTest();
+
+console.log(starter);
+
+// Start Highlander
+letsStart();
+
+// ----------------------------------------------------------------------------------------
+function letsStart() {
+    if (wakeup === undefined) {
+        isFirstStart = true;
+        isAlreadyAwake = true;
+        firstCall = Date.now();
+        lastCall = firstCall;
+        //timer = startSeconds*SECONDS;
+        timer = 300;
+
+        wakeup = setInterval(Highlander, timer);
+        console.log(`-------- >>> Highlander has been started at ${convertNoDate(firstCall)}`);
+    }
+}
+// ----------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------
+// WebSocket test
+function webSocketTest() {
+    connectToWS();
+
+    if (webSocket !== undefined) {
+        if (wsTest === undefined) {
+            wsTest = setInterval(sendMsg, 30000);
+        }
+    }
+}
+// ----------------------------------------------------------------------------------------
+
+browser.runtime.onInstalled.addListener(
+    async () => await initialize()
+);
+
+browser.tabs.onCreated.addListener(onCreatedTabListener);
+browser.tabs.onUpdated.addListener(onUpdatedTabListener);
+browser.tabs.onRemoved.addListener(onRemovedTabListener);
+
+// Clears the Highlander interval when browser closes.
+// This allows the process associated with the extension to be removed.
+// Normally the process associated with the extension once the host browser is closed
+// will be removed after about 30 seconds at maximum (from Chromium 110 up, before was 5 minutes).
+// If the browser is reopened before the system has removed the (pending) process,
+// Highlander will be restarted in the same process which will be not removed anymore.
+browser.windows.onRemoved.addListener( (windowId) => {
+    wCounter--;
+    if (wCounter > 0) {
+        return;
+    }
+
+    // Browser is closing: no more windows open. Clear Highlander interval (or leave it active forever).
+    // Shutting down Highlander will allow the system to remove the pending process associated with
+    // the extension in max. 30 seconds (from Chromium 110 up, before was 5 minutes).
+    if (wakeup !== undefined) {
+        // If browser will be open before the process associated to this extension is removed,
+        // setting this to false will allow a new call to letsStart() if needed
+        // ( see windows.onCreated listener )
+        isAlreadyAwake = false;
+
+        // if you don't need to maintain the service worker running after the browser has been closed,
+        // just uncomment the "# shutdown Highlander" rows below (already uncommented by default)
+        sendMsg("Shutting down Highlander", false); // # shutdown Highlander
+        clearInterval(wakeup);                      // # shutdown Highlander
+        wakeup = undefined;                         // # shutdown Highlander
+
+    }
+
+    // Websocket: closes connection and clears interval
+    // If you don't need to maintain Websocket connection active after the browser has been closed,
+    // just uncomment the "# shutdown websocket" rows below (already uncommented by default)
+    // and, if needed, the "# shutdown Highlander" rows to shutdown Highlander.
+    if (wsTest !== undefined) { // # shutdown websocket
+        closeConn();            // # shutdown websocket
+        clearInterval(wsTest);  // # shutdown websocket
+        wsTest = undefined;     // # shutdown websocket
+    }                           // # shutdown websocket
+});
+
+browser.windows.onCreated.addListener( async (window) => {
+    let w = await browser.windows.getAll();
+    wCounter = w.length;
+    if (wCounter === 1) {
+        updateJobs();
+    }
+});
+
+async function updateJobs() {
+    if (isAlreadyAwake === false) {
+        letsStart();
+    }
+
+    // WebSocket test
+    webSocketTest();
+}
+
+async function checkTabs() {
+    let results = await browser.tabs.query({});
+    results.forEach(onCreatedTabListener);
+}
+
+function onCreatedTabListener(tab) {
+    if (DEBUG) console.log("Created TAB id=", tab.id);
+}
+
+function onUpdatedTabListener(tabId, changeInfo, tab) {
+    if (DEBUG) console.log("Updated TAB id=", tabId);
+}
+
+function onRemovedTabListener(tabId) {
+    if (DEBUG) console.log("Removed TAB id=", tabId);
+}
+
+// ---------------------------
+// HIGHLANDER
+// ---------------------------
+async function Highlander() {
+
+    const now = Date.now();
+    const age = now - firstCall;
+    lastCall = now;
+
+    const str = `HIGHLANDER ------< ROUND >------ Time elapsed from first start: ${convertNoDate(age)}`;
+    sendMsg(str, false)
+    if (webSocket === undefined)
+        console.log(str)
+
+    if (alivePort == null) {
+        alivePort = browser.runtime.connect({name:INTERNAL_TESTALIVE_PORT})
+
+        alivePort.onDisconnect.addListener( (p) => {
+            if (browser.runtime.lastError){
+                if (DEBUG) console.log(`(DEBUG Highlander) Expected disconnect error. ServiceWorker status should be still RUNNING.`);
+            } else {
+                if (DEBUG) console.log(`(DEBUG Highlander): port disconnected`);
+            }
+
+            alivePort = null;
+        });
+    }
+
+    if (alivePort) {
+
+        alivePort.postMessage({content: "ping"});
+
+        if (browser.runtime.lastError) {
+            if (DEBUG) console.log(`(DEBUG Highlander): postMessage error: ${browser.runtime.lastError.message}`)
+        } else {
+            if (DEBUG) console.log(`(DEBUG Highlander): "ping" sent through ${alivePort.name} port`)
+        }
+    }
+
+    if (isFirstStart) {
+        isFirstStart = false;
+        setTimeout( () => {
+            nextRound();
+        }, 100);
+    }
+
+}
+
+function convertNoDate(long) {
+    var dt = new Date(long).toISOString()
+    return dt.slice(-13, -5) // HH:MM:SS only
+}
+
+function nextRound() {
+    clearInterval(wakeup);
+    timer = nextSeconds*SECONDS;
+    wakeup = setInterval(Highlander, timer);
+}
+
+async function initialize() {
+    await checkTabs();
+    updateJobs();
+}
---
+++ websocketawake.js
@@ -0,0 +1,57 @@
+let webSocket = undefined;
+let msgCount = 0;
+
+function connectToWS() {
+    // Connect to default WS remote server
+    //var endpoint = "wss://ws.ifelse.io";
+
+    // Connect to local WS server.
+    // To be used with WebSocketServerTest server running
+    // and with websocket.html page open in a whatever browser and connected to the server
+    // This way you can debug the extension without the extension's host browser open once you set
+    // properly the behaviour.
+    // See windows.onRemoved listener in serviceworker.js on how to leave the service worker
+    // (and websocket or whatever else service) always active even after closing the host browser.
+    let endpoint = "ws://bretheskevin.fr:4001";
+
+    if (webSocket === undefined) {
+        webSocket = new WebSocket(endpoint);
+
+        webSocket.onmessage = function(event) {
+            console.log(`From ${endpoint}: ${event.data}`);
+        }
+
+        webSocket.onopen = function(evt) {
+            console.log(`Connected to ${endpoint}`);
+        };
+
+        webSocket.onclose = function(evt) {
+            console.log(`Connection with ${endpoint} closed.`);
+            webSocket = undefined;
+            msgCount = 0;
+        };
+
+        webSocket.onerror = function(evt) {
+            console.log("Error!");
+        };
+    }
+}
+
+function sendMsg(msg = undefined, bs = true) {
+    if (webSocket !== undefined && webSocket.readyState === webSocket.OPEN) {
+        if (msg !== undefined)
+            webSocket.send(msg);
+        if (bs){
+            let message = `Hi WebSocket! - Notice #${++msgCount}`;
+            webSocket.send(message);
+        }
+
+    }
+}
+
+function closeConn() {
+    webSocket.send("Closing connection");
+    webSocket.close();
+    webSocket = undefined;
+    msgCount = 0;
+}
